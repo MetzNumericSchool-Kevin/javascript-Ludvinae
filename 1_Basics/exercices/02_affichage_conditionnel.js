5135ab2d232aaf025d9cbc321a16ee56c2b142c4
unknown <benoit.champlon79@gmail.com>
2026-02-03 09:15:14 +0100

    02_affichage_conditionnel

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -1,5 +1,11 @@
 // Affichage conditionnel
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
-const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+const nom_boutique = "L'incoyable Bric-à-brac sans broc";
+const boutique_ouverte = false;
+
+if (boutique_ouverte) {
+    console.log(`Bienvenue à ${nom_boutique}!`);
+} else {
+    console.log(`${nom_boutique} est fermée, revenez plus tard.`)
+}
