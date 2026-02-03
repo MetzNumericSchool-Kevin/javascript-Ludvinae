ce54ccf198eab3718acf9ecb33cdc6a5163fdb23
unknown <benoit.champlon79@gmail.com>
2026-02-03 11:00:00 +0100

    08_ajout_nouvelle_potion

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -2,3 +2,13 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+potions.push("Potion d'antidote");
+potions.forEach(element => {
+    console.log(element);
+});
+
+potions.pop();
+potions.forEach(element => {
+    console.log(element);
+});
