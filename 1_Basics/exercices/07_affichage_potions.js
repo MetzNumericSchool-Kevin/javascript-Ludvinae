dba9a7d36027b8d909719f8b0639101547bac425
unknown <benoit.champlon79@gmail.com>
2026-02-03 10:56:57 +0100

    07_affichage_potions

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -2,3 +2,10 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+console.log(potions[0]);
+console.log(potions[potions.length - 1]);
+
+potions.forEach(element => {
+    console.log(`Nous avons de la ${element}!`)
+});
