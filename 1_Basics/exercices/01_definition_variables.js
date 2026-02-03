033111bb4f6d644a88454c46f15ff5fb35e391ab
unknown <benoit.champlon79@gmail.com>
2026-02-03 09:01:40 +0100

    01_definition_variables

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,14 @@
 // Définition de variables
+
+const nomBoutique = "L'incoyable Bric-à-brac sans broc";
+let stockPotionSoin = 18;
+let prixPotionSoin = 100;
+let estOuvert = true;
+
+
+if (estOuvert) {
+    console.log(`La boutique ${nomBoutique} est ouverte.`);
+} else {
+    console.log(`La boutique ${nomBoutique} est fermée.`);
+}
+console.log(`Il y'a ${stockPotionSoin} potions de soins en stock, au prix unitaire de ${prixPotionSoin}`);
