871c02fa9bd510dbf2f4bcf5eb975c9b56a4fbe2
unknown <benoit.champlon79@gmail.com>
2026-02-03 11:11:43 +0100

    10_objet_potion_soin

--- 1_Basics/exercices/10_objet_potion_soin.js
+++ 1_Basics/exercices/10_objet_potion_soin.js
@@ -1,1 +1,9 @@
 // Rangeons les informations de la potion de soin dans un objet 🧹
+
+const potionSoins = {
+    nom: "Potion de soins",
+    prix: 50,
+    stock: 18
+};
+
+console.log(potionSoins);
