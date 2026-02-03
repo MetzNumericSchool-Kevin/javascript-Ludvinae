ad08b466b2fb4f8c201bc8d6898828edac931b18
unknown <benoit.champlon79@gmail.com>
2026-02-03 09:57:44 +0100

    04_calcul_prix_total

--- 1_Basics/exercices/04_calcul_prix_total.js
+++ 1_Basics/exercices/04_calcul_prix_total.js
@@ -2,3 +2,7 @@
 
 // Variables des exercices précédents (modifie selon tes valeurs)
 let prix_potion_soin = 50;
+
+let choix = prompt(`Combien de potions de soins voulez-vous acheter? `)
+
+console.log(`Prix de ${choix} potions: ${Number.parseInt(choix) * prix_potion_soin}.`)
