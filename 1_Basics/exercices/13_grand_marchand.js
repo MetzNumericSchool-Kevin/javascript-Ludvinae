2289c9fa5bcef7bcfb854db1d8f76a23f32ec5dc
unknown <benoit.champlon79@gmail.com>
2026-02-04 08:59:15 +0100

    13_grand_marchandage

--- 1_Basics/exercices/13_grand_marchand.js
+++ 1_Basics/exercices/13_grand_marchand.js
@@ -7,3 +7,29 @@ const inventaire = [
   { nom: "Potion d'endurance", prix: 40, stock: 0 },
   { nom: "Potion de force", prix: 60, stock: 3 },
 ];
+
+prixMax = 0;
+potionPlusChere = null;
+inventaire.forEach(potion => {
+  if (potion.prix > prixMax) {
+    prixMax = potion.prix;
+    potionPlusChere = potion;
+  }
+})
+console.log(potionPlusChere);
+
+stockMax = 0;
+potionPlusStockee = null;
+inventaire.forEach(potion => {
+  if (potion.stock > stockMax) {
+    stockMax = potion.prix;
+    potionPlusStockee = potion;
+  }
+})
+console.log(potionPlusStockee);
+
+let potionEnStock = 0;
+inventaire.forEach(potion => {
+  if (potion.stock > 0) potionEnStock += 1;
+})
+console.log(potionEnStock)
