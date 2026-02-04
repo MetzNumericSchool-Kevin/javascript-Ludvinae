05f272ec70c3a0ce0920d4de628108e1904fa6fe
unknown <benoit.champlon79@gmail.com>
2026-02-04 09:15:15 +0100

    14_filtrage_selection

--- 1_Basics/exercices/14_filtrage_selection.js
+++ 1_Basics/exercices/14_filtrage_selection.js
@@ -8,3 +8,17 @@ const inventaire = [
   { nom: "Potion de force", prix: 60, stock: 3 },
   { nom: "Potion de vitesse", prix: 35, stock: 8 }
 ];
+
+const potionsEnStock = [];
+const potionsAbordables = [];
+
+inventaire.forEach(potion => {
+  if (potion.stock > 0) potionsEnStock.push(potion);
+  if (potion.prix < 40) potionsAbordables.push(potion);
+})
+
+console.log("potions en stock: ", potionsEnStock.length)
+console.log("potions avec un prix inferieur a 40: ", potionsAbordables.length);
+
+console.log(potionsEnStock);
+console.log(potionsAbordables);
